be43652495c784595165d7aa312ca5060295c4e2
missing-user <philipp.jurasic@gmx.de>
2020-05-26 22:57:33 +0200

    clearing timeouts, grid animations

---
+++ web/scripts/animations.js
@@ -0,0 +1,86 @@
+const staggerVisualizerEl = document.getElementById("stagger-visualizer");
+const staggerVisualizerEl2 = document.getElementById("stagger-visualizer2");
+const staggerVisualizerEl3 = document.getElementById("stagger-visualizer3");
+const fragment = document.createDocumentFragment();
+const grid = [14, 14];
+const col = grid[0];
+const row = grid[1];
+const numberOfElements = col * row;
+for (let i = 0; i < numberOfElements; i++) {
+	fragment.appendChild(document.createElement("div"));
+}
+staggerVisualizerEl.appendChild(fragment);
+for (let i = 0; i < numberOfElements; i++) {
+	fragment.appendChild(document.createElement("div"));
+}
+staggerVisualizerEl2.appendChild(fragment);
+for (let i = 0; i < numberOfElements; i++) {
+	fragment.appendChild(document.createElement("div"));
+}
+staggerVisualizerEl3.appendChild(fragment);
+
+function animateBeat(duration) {
+	anime({
+		targets: "#stagger-visualizer2 div",
+		easing: 'easeInOutSine',
+		loop: false,
+		autoplay: true,
+		delay: anime.stagger(duration / row / 2, {
+			grid: grid,
+			from: 'center'
+		}),
+		scale: [{
+			value: .1,
+			easing: 'easeOutSine',
+			duration: duration / 4
+		}, {
+			value: 1,
+			easing: 'easeInOutQuad',
+			duration: duration / 2
+		}],
+	})
+}
+
+function animateBar(duration) {
+	anime({
+		targets: "#stagger-visualizer div",
+		easing: 'easeInOutSine',
+		loop: false,
+		autoplay: true,
+		delay: anime.stagger(duration / row / 2, {
+			grid: grid,
+			from: 'first'
+		}),
+		scale: [{
+			value: .1,
+			easing: 'easeOutSine',
+			duration: duration / 4
+		}, {
+			value: 1,
+			easing: 'easeInOutQuad',
+			duration: duration / 2
+		}],
+	})
+}
+
+function animateSection(duration) {
+	anime({
+		targets: "#stagger-visualizer3 div",
+		easing: 'easeInOutSine',
+		loop: false,
+		autoplay: true,
+		delay: anime.stagger(duration / row / 2, {
+			grid: grid,
+			from: 'last'
+		}),
+		scale: [{
+			value: .1,
+			easing: 'easeOutSine',
+			duration: duration / 4
+		}, {
+			value: 1,
+			easing: 'easeInOutQuad',
+			duration: duration / 4
+		}],
+	})
+}
