fd30f202b1e2e3a2aec96baba1210685d74e8c7d
missing-user <philipp.jurasic@gmx.de>
2020-05-28 12:33:26 +0200

    added rudimentary color support and moar effects

--- web/scripts/animations.js
+++ web/scripts/animations.js
@@ -61,33 +61,84 @@ setInterval(function() {
 	// TODO: since the effect are timed ina a way to be completely offscreen by the
 	// time the bar is over, their timing isnt in sync with the beats, eventhough 4 beats = 4 arrows
 	//(but the arrows are over fast and then have a pause while the beats are evenly spaced)
-	if (even) Effects.arrow(timePercent, CurrentMusic.time_signature);
-	else
-		Effects.wave(
-			timePercent,
-			CurrentMusic.time_signature,
-			EasingFunctions.linear
-		);
+	switch (effect % 14) {
+		case 1:
+			Effects.circleInwards(timePercent);
+			break;
+		case 2:
+			Effects.arrow(timePercent, CurrentMusic.time_signature);
+			break;
+		case 3:
+			Effects.circleInwardsSharp(timePercent);
+			break;
+		case 4:
+			Effects.wave(timePercent, CurrentMusic.time_signature);
+			break;
+		case 5:
+			Effects.squareInwardsSharp(timePercent);
+			break;
+		case 6:
+			Effects.curtain(timePercent, EasingFunctions.easeOutCubic);
+			break;
+		case 7:
+			Effects.squareInwards(timePercent);
+			break;
+		case 8:
+			Effects.squareInwardsSharp(timePercent, EasingFunctions.square);
+			break;
+		case 9:
+			Effects.curtain(timePercent);
+			break;
+		case 10:
+			Effects.diagonalWave(timePercent, CurrentMusic.time_signature);
+			break;
+		case 11:
+			Effects.diagonalWave(
+				timePercent,
+				CurrentMusic.time_signature,
+				EasingFunctions.square
+			);
+			break;
+		case 12:
+			Effects.diagonalWave(
+				timePercent,
+				CurrentMusic.time_signature,
+				EasingFunctions.easeInOutCubic
+			);
+			break;
+		case 13:
+			Effects.arrow(
+				timePercent,
+				CurrentMusic.time_signature,
+				EasingFunctions.easeInOutCubic
+			);
+			break;
+		default:
+			Effects.arrow(
+				timePercent,
+				CurrentMusic.time_signature,
+				EasingFunctions.spikeInCubic
+			);
+	}
 
 	leds = rotateMatrix90(rotation);
 	drawCanvasMatrix();
 }, 10);
 
-function getCoverColors() {
-	pix = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-
-	for (var i = 0; i < pix.length; i += 4 * 16) {
-		leds[i / 16].r = pix[i];
-		leds[i / 16].g = pix[i + 1];
-		leds[i / 16].b = pix[i + 2];
+function fillColor(r, g, b) {
+	for (var led of leds) {
+		led.r = r;
+		led.g = g;
+		led.b = b;
 	}
 }
 
 function drawCanvasMatrix() {
 	size = 50;
 	margin = 10;
-	ctx2.fillStyle = "white";
-	ctx2.clearRect(0, 0, canvasMatrix.width, canvasMatrix.height);
+	ctx2.fillStyle = "#2e3436";
+	ctx2.fillRect(0, 0, canvasMatrix.width, canvasMatrix.height);
+
 	for (var i = 0; i < leds.length; i++) {
 		offset = (size * leds[i].v) / 2;
 
@@ -104,8 +155,8 @@ function drawCanvasMatrix() {
 		ctx2.fillRect(
 			(XY(i).x + 0.5) * (size + margin) - offset,
 			(XY(i).y + 0.5) * (size + margin) - offset,
-			size * leds[i].v,
-			size * leds[i].v
+			size * leds[i].v + 4,
+			size * leds[i].v + 4
 		);
 	}
 }
@@ -126,10 +177,65 @@ Effects = {
 			leds[i].v = ease(Math.clamp(eval > iterations ? eval : eval % 1));
 		}
 	},
+	diagonalWave: (
+		timePercent,
+		iterations = 1,
+		ease = EasingFunctions.gaussianFit
+	) => {
+		for (var i = 0; i < numberOfElements; i++) {
+			eval = timePercent * (iterations + 2) - 2 + XY(i).x / row + XY(i).y / col;
+			leds[i].v = ease(Math.clamp(eval > iterations ? eval : eval % 1));
+		}
+	},
 	wave: (timePercent, iterations = 1, ease = EasingFunctions.gaussianFit) => {
 		for (var i = 0; i < leds.length; i++) {
 			eval = timePercent * (iterations + 1) - 1 + XY(i).x / row;
 			leds[i].v = ease(Math.clamp(eval > iterations ? eval : eval % 1));
+		}
+	},
+	squareInwardsSharp: (timePercent, ease = EasingFunctions.linear) => {
+		eval = (timePercent * row) / 2;
+		for (var i = 0; i < leds.length; i++) {
+			distFromCenter = Math.max(
+				Math.abs((col - 1) / 2 - XY(i).y),
+				Math.abs((row - 1) / 2 - XY(i).x)
+			);
+			leds[i].v = ease(Math.clamp(eval - distFromCenter + 0.5));
+		}
+	},
+	squareInwards: (timePercent, ease = EasingFunctions.spikeInCubic) => {
+		for (var i = 0; i < leds.length; i++) {
+			distFromCenter = Math.max(
+				Math.abs((col - 1) / 2 - XY(i).y),
+				Math.abs((row - 1) / 2 - XY(i).x)
+			);
+			leds[i].v = ease(Math.clamp(timePercent * 1.2 - distFromCenter / row));
+		}
+	},
+	curtain: (timePercent, ease = EasingFunctions.spikeInCubic) => {
+		for (var i = 0; i < leds.length; i++) {
+			distFromCenter = Math.abs((row - 1) / 2 - XY(i).x);
+			leds[i].v = ease(Math.clamp(timePercent * 1.2 - distFromCenter / row));
+		}
+	},
+	circleInwards: (timePercent, ease = EasingFunctions.spikeInCubic) => {
+		eval = timePercent * 1.4143; //hardcoded root(2)
+		for (var i = 0; i < leds.length; i++) {
+			dy = (col - 1) / 2 - XY(i).y;
+			dx = (col - 1) / 2 - XY(i).x;
+			distFromCenter = Math.sqrt(dy * dy + dx * dx);
+
+			leds[i].v = ease(Math.clamp(eval - distFromCenter / row));
+		}
+	},
+	circleInwardsSharp: (timePercent, ease = EasingFunctions.linear) => {
+		eval = (timePercent * row * 1.4143) / 2; //hardcoded root(2)
+		for (var i = 0; i < leds.length; i++) {
+			dy = (col - 1) / 2 - XY(i).y;
+			dx = (col - 1) / 2 - XY(i).x;
+			distFromCenter = Math.sqrt(dy * dy + dx * dx);
+
+			leds[i].v = ease(Math.clamp(eval - distFromCenter + 0.5));
 		}
 	}
 };
@@ -144,6 +250,8 @@ EasingFunctions = {
 	inverse: t => 1 - t,
 	// no easing, no acceleration
 	linear: t => t,
+	// always one exept for on the edges
+	square: t => (t <= 0 || t >= 1 ? 0 : 1),
 	// accelerating from zero velocity
 	easeInQuad: t => t * t,
 	// decelerating to zero velocity
