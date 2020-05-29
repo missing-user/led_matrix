a10444c692386b50871d85dffc0c870604318560
missing-user <philipp.jurasic@gmx.de>
2020-05-29 14:30:04 +0200

    i give up

--- python/main.py
+++ python/main.py
@@ -1,21 +1,43 @@
+import sched
+import time
+from tkinter import *
+
 from PIL import Image
 
 TESTING = True
 
 if(TESTING):
-    import preview
-    display = preview.Preview_display(16, 16)
+    import preview as display
+    from tkinter import *
+    master = Tk()
+    # the canvas fits matrix and has a margin of [pixel_spacing] around the border
+    w = Canvas(master, width=20 + 16 * 50,
+               height=20 + 16 * 50)
+    w.pack()
+    w.create_rectangle(10, 20, 20, 30, fill="#FFF")
+    display.generateDisplay(w, 10, 50, 16, 16)
 else:
     import Led_display
     display = Led_display(16, 16)
 
 
-try:
+# try:
     # Relative Path
-    img = Image.open("../web/images/256icon.png")
+#    img = Image.open("../web/images/256icon.png")
     # In-place modification
-    img.thumbnail((16, 16))
-    img.save("thumb.png")
-    display.drawPixels(range(100))
-except IOError:
-    pass
+#    img.thumbnail((16, 16))
+#    img.save("thumb.png")
+# except IOError:
+#    pass
+
+#s = sched.scheduler(time.time, time.sleep)
+
+
+def loop(sc):
+    print("Doing stuff...")
+    # do your stuff
+    s.enter(0.1, 1, loop, (sc,))
+
+
+#s.enter(0.1, 1, loop, (s,))
+# s.run()
--- python/preview.py
+++ python/preview.py
@@ -1,29 +1,22 @@
-from tkinter import *
+pixels = []
 
 
-class Preview_display:
-    """Preview_display is used to visualize effects during production, without connecting a matrix"""
+def generateDisplay(window, spacing, size, width, height):
+    m_width = height
+    m_height = width
 
-    def __init__(self, width, height):
-        super(Preview_display, self).__init__()
-        self.m_width = width
-        self.m_height = height
-        master = Tk()
+    pixel_size = size
+    pixel_spacing = spacing
 
-        pixel_size = 50
-        pixel_spacing = 10
-        self.pixels = []
+    print(pixel_size)
 
-        # the canvas fits matrix and has a margin of [pixel_spacing] around the border
-        self.w = Canvas(master, width=2 * pixel_spacing + self.m_width * pixel_size,
-                        height=2 * pixel_spacing + self.m_height * pixel_size)
-        self.w.pack()
-        for x in range(pixel_spacing, pixel_spacing + self.m_width * pixel_size, pixel_size):
-            for y in range(pixel_spacing, pixel_spacing + self.m_height * pixel_size, pixel_size):
-                self.pixels.append(self.w.create_rectangle(
-                    x, y, x + pixel_size, y + pixel_size, fill="#FFF"))
+    for x in range(pixel_spacing, pixel_spacing + m_width * pixel_size, pixel_size):
+        for y in range(pixel_spacing, pixel_spacing + m_height * pixel_size, pixel_size):
+            window.create_rectangle(
+                x, y, x + pixel_size, y + pixel_size, fill="#FFF")
 
-    def drawPixels(pixelData):
-        for pixel in pixelData:
-            self.w.itemconfig(i, fill="blue")
-            print(pixel)
+
+def drawPixels(pixelData):
+    for idx, pixel in pixelData:
+        w.itemconfig(idx, fill="blue")
+        print(pixel)
